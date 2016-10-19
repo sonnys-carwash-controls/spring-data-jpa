65e338d49f55ec4143bc5614f265685466003a58
Brian Chase <bchase@sonnysdirect.com>
2016-10-19 14:25:04 -0400

    Added Sonnys patch

--- src/main/java/org/springframework/data/jpa/repository/support/JpaMetamodelEntityInformation.java
+++ src/main/java/org/springframework/data/jpa/repository/support/JpaMetamodelEntityInformation.java
@@ -323,36 +323,50 @@ public class JpaMetamodelEntityInformation<T, ID extends Serializable> extends J
 		@Override
 		public void setPropertyValue(String propertyName, Object value) {
 
-			if (!isIdentifierDerivationNecessary(value)) {
-				super.setPropertyValue(propertyName, value);
+//			if (!isIdentifierDerivationNecessary(value)) {
+//				super.setPropertyValue(propertyName, value);
+//				return;
+//			}
+//
+//			// Derive the identifier from the nested entity that is part of the composite key.
+//			@SuppressWarnings({ "rawtypes", "unchecked" })
+//			JpaMetamodelEntityInformation nestedEntityInformation = new JpaMetamodelEntityInformation(value.getClass(),
+//					this.metamodel);
+//
+//			if (!nestedEntityInformation.getJavaType().isAnnotationPresent(IdClass.class)) {
+//
+//				Object nestedIdPropertyValue = new DirectFieldAccessFallbackBeanWrapper(value)
+//						.getPropertyValue(nestedEntityInformation.getIdAttribute().getName());
+//				super.setPropertyValue(propertyName, nestedIdPropertyValue);
+//				return;
+//			}
+//
+//			// We have an IdClass property, we need to inspect the current value in order to map potentially multiple id
+//			// properties correctly.
+//
+//			BeanWrapper sourceIdValueWrapper = new DirectFieldAccessFallbackBeanWrapper(value);
+//			BeanWrapper targetIdClassTypeWrapper = new BeanWrapperImpl(nestedEntityInformation.getIdType());
+//
+//			for (String idAttributeName : (Iterable<String>) nestedEntityInformation.getIdAttributeNames()) {
+//				targetIdClassTypeWrapper.setPropertyValue(idAttributeName,
+//						extractActualIdPropertyValue(sourceIdValueWrapper, idAttributeName));
+//			}
+//
+//			super.setPropertyValue(propertyName, targetIdClassTypeWrapper.getWrappedInstance());
+			
+			if (isIdentifierDerivationNecessary(value)) {
+				 
+				// Derive the identifer from the nested entity that is part of the composite key.
+				@SuppressWarnings({ "rawtypes", "unchecked" })
+				JpaMetamodelEntityInformation nestedEntityInformation = new JpaMetamodelEntityInformation(value.getClass(),
+						this.metamodel);
+				
+				super.setPropertyValue(propertyName, nestedEntityInformation.getId(value));
 				return;
 			}
-
-			// Derive the identifier from the nested entity that is part of the composite key.
-			@SuppressWarnings({ "rawtypes", "unchecked" })
-			JpaMetamodelEntityInformation nestedEntityInformation = new JpaMetamodelEntityInformation(value.getClass(),
-					this.metamodel);
-
-			if (!nestedEntityInformation.getJavaType().isAnnotationPresent(IdClass.class)) {
-
-				Object nestedIdPropertyValue = new DirectFieldAccessFallbackBeanWrapper(value)
-						.getPropertyValue(nestedEntityInformation.getIdAttribute().getName());
-				super.setPropertyValue(propertyName, nestedIdPropertyValue);
-				return;
-			}
-
-			// We have an IdClass property, we need to inspect the current value in order to map potentially multiple id
-			// properties correctly.
-
-			BeanWrapper sourceIdValueWrapper = new DirectFieldAccessFallbackBeanWrapper(value);
-			BeanWrapper targetIdClassTypeWrapper = new BeanWrapperImpl(nestedEntityInformation.getIdType());
-
-			for (String idAttributeName : (Iterable<String>) nestedEntityInformation.getIdAttributeNames()) {
-				targetIdClassTypeWrapper.setPropertyValue(idAttributeName,
-						extractActualIdPropertyValue(sourceIdValueWrapper, idAttributeName));
-			}
-
-			super.setPropertyValue(propertyName, targetIdClassTypeWrapper.getWrappedInstance());
+			
+			super.setPropertyValue(propertyName, value);
+			
 		}
 
 		private Object extractActualIdPropertyValue(BeanWrapper sourceIdValueWrapper, String idAttributeName) {
